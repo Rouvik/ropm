3f292ea5f93a00ff79554f8f9d7a68b3b392035e
Rouvik Maji <majirouvik@gmail.com>
2025-08-11 00:02:26 +0530

    Forgot to update repo install url :(

--- include/repoDownload.h
+++ include/repoDownload.h
@@ -1,6 +1,6 @@
 #ifndef INCLUDED_REPODOWNLOAD_H
 #define INCLUDED_REPODOWNLOAD_H
 
-#define downloadRepoURL "http://localhost:8080/"
+#define downloadRepoURL "https://rouvik.github.io/ropm/"
 
 #endif // INCLUDED_REPODOWNLOAD_H
